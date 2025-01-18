d6fd38b731e0517626d0ddb445fa85cb383d7ee2
Codedecblackmoon <gideonlenge96@gmail.com>
2025-01-18 13:39:30 +0200

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Sorry... Demo is not available</h2>
+        <h1>Sorry...</h1>
+        <h3>The demo for this project is currently unavailable. <br/> Please consider cloning it from my GitHub repository and running it locally</h3>
         <div className='links'>
           <a className="App-link" href="https://github.com/Codedecblackmoon" target="_blank"> My Github </a>
           <a className="App-link" href="https://my-portfolio-ten-psi-47.vercel.app/" target="_blank"> Back to Portfolio </a>
